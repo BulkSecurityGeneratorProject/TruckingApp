e652040889aff0179f857005a60ea5f16210af4e
Alex <pavlovskiy.alexey.work@gmail.com>
2017-11-15 19:51:16 +0300

    Add Item and ItemConsignment POJO classes to domain folder

---
+++ src/main/java/com/itechart/domain/Item.java
@@ -0,0 +1,73 @@
+package com.itechart.domain;
+
+public class Item {
+
+    private Integer id;
+    private String name;
+    private Integer price;
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Item item = (Item) o;
+
+        if (!id.equals(item.id)) return false;
+        if (name != null ? !name.equals(item.name) : item.name != null) return false;
+        if (price != null ? !price.equals(item.price) : item.price != null) return false;
+        return type != null ? type.equals(item.type) : item.type == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", price=" + price +
+            ", type='" + type + '\'' +
+            '}';
+    }
+}
