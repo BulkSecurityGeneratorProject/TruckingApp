21852ee4a97bb603d4fd4796797fe21d9931c839
Alex <pavlovskiy.alexey.work@gmail.com>
2017-11-16 02:31:19 +0300

    Adding DAO to Item model

---
+++ src/main/java/com/itechart/dao/ItemDAO.java
@@ -0,0 +1,19 @@
+package com.itechart.dao;
+
+import com.itechart.domain.Item;
+
+import java.util.List;
+
+public interface ItemDAO {
+
+    List<Item> getAllItems(int page, int numbers);
+
+    Item getItemById(int id);
+
+    Item getItemByName(String name);
+
+    void deleteItem(int id);
+
+    void saveItem(String name);
+
+}
